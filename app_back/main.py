3391e8a2b2726919b1f6253100bcf8b1886df5c1
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2025-01-17 12:54:42 +0100

    test

--- app_back/main.py
+++ app_back/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request # type: ignore
 import os
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -9,7 +10,20 @@ tasks = {
         'completed': False  # Por defecto, la tarea no está completada
     }
 
+API_TOKEN = os.getenv("API_TOKEN")
+print(API_TOKEN)
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token or token != f"Bearer {API_TOKEN}":
+            return jsonify({'error': 'Token inválido o no proporcionado'}), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/tasks', methods=['POST'])
+@token_required
 def create_task():
     """Crear una nueva tarea"""
     data = request.get_json()
@@ -25,6 +39,7 @@ def create_task():
     return jsonify(new_task), 201
 
 @app.route('/tasks/<int:task_id>', methods=['POST'])
+@token_required
 def update_task(task_id):
     """Actualizar una tarea existente"""
     data = request.get_json()
@@ -41,6 +56,7 @@ def update_task(task_id):
     return jsonify(task)
 
 @app.route('/tasks/delete/<int:task_id>', methods=['POST'])
+@token_required
 def delete_task(task_id):
     """Eliminar una tarea por su ID"""
     global tasks
@@ -53,6 +69,7 @@ def delete_task(task_id):
     return jsonify({'message': 'Tarea eliminada exitosamente'}), 200
 
 @app.route('/tasks/list', methods=['POST'])
+@token_required
 def list_tasks():
     """Listar todas las tareas"""
     return jsonify(tasks), 200
